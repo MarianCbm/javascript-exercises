e413e8f0e92ca3d281c53976f19a2dcdc937af43
MarianCbm <cioara.mariann@yahoo.com>
2023-07-09 13:45:31 +0300

    Add my solution to exercise 05 after all tests passed successfully

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstInteger, secondInteger) {
+    let sum=0;
+    if ( firstInteger >= 0 && secondInteger >= 0 && Number.isInteger(firstInteger) && Number.isInteger(secondInteger) ) {
+        if (firstInteger===secondInteger) {
+            sum=firstInteger;
+            return sum;
+        }
+        else if (firstInteger<secondInteger) {
+            for (let i=firstInteger; i<=secondInteger; i++){
+                sum=sum + i;
+            }
+            return sum;
+        }
+        else {
+            for (let i=secondInteger; i<=firstInteger; i++)
+            sum=sum+i;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
+    
 
 };
 
