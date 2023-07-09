e0e132a812f5ae61deed34c8ebb2297b16a3441b
MarianCbm <cioara.mariann@yahoo.com>
2023-07-09 14:27:45 +0300

    Add my solution to #7 exercise after all tests passed successfully

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  if(typeof(fahrenheitTemp)=="number"){
+    finalTemp=(fahrenheitTemp-32)/1.8;
+    finalTemp=Math.round(finalTemp*10)/10;
+    return finalTemp;
+  }
+  else {
+    return "ERROR"
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  if(typeof(celsiusTemp=="number")){
+    finalTemp= celsiusTemp*1.8 +32;
+    finalTemp=Math.round(finalTemp*10)/10;
+    return finalTemp;
+  }
+  else {
+    return "ERROR"
+  }
 };
 
 // Do not edit below this line
