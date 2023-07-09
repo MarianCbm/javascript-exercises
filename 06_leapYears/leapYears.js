24f43584c5899c2ce34c22dd562a8115d6233674
MarianCbm <cioara.mariann@yahoo.com>
2023-07-09 14:03:17 +0300

    Add my solution to the #7 exercise adter all tests passed successfully

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (Number.isInteger(year)) {
+        if(year%4===0) {
+            if (year%100===0 && year%400===0){
+                return true;
+            }
+            else if (year%100===0) {
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
 
 };
 
